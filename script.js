7fde8f437c87e3c02f1240f56bbb9b94549e9a6b
alireza-sadi <alirezasadi80@gmail.com>
2022-11-02 22:28:34 +0330

    update script - not completed

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ function showMenu(el) {
     s.style.opacity = "0";
   });
 }
-navBar = document.querySelector(".navbar")
+navBar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", () => {
-    navBar.style.position = "sticky"
-    navBar.style.backgroundImage = "var(--background-gradient)";
-})
+const sticky = navBar.offsetTop + navBar.offsetHeight;
+
+if (window.scrollY >= sticky) {
+  console.log("yes")
+  navBar.style.position = "sticky";
+  navBar.style.backgroundImage = "var(--background-gradient)";
+} else {
+    console.log("no")
+    navBar.style.position = "absolute " ;
+    navBar.style.backgroundImage = "transparent";
+}
+  console.log(sticky)
+// window.addEventListener("scroll", () => {
+//   navBar.style.position = "sticky";
+//   navBar.style.backgroundImage = "var(--background-gradient)";
+// });
